225b8438f1a123b32fd4ba042039bad09444f68e
Guilherme Tavares Pinheiro <gui.tavares2002.contato@gmail.com>
2022-06-14 14:43:18 -0300

    Update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,10 @@ import React from 'react'
 export default function Footer() {
     return(
         <footer>
-            <small><a href='https://github.com/guilherme-tavares-py'>Guilherme Tavares</a></small>
+            <small>
+                <a href="https://github.com/guilherme-tavares-py" class="fa fa-github"></a>
+                <a href="https://www.linkedin.com/in/guilherme-tavares-pinheiro-21510b1b1/" class="fa fa-linkedin"></a>
+            </small>
         </footer>
     )
 }
